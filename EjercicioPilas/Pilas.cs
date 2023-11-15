e8859a5bcc80a31e7e90af46cb8e6d420596106c
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2023-11-15 12:37:08 -0700

    Agregar archivos de proyecto.

---
+++ EjercicioPilas/Pilas.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EjercicioPilas
+{
+    public class Pilas
+    {
+        private int _cima;
+        private int [] listaPila;
+        private int _longitudPila = 5;
+
+        public Pilas()
+        {
+            _cima = -1;
+            listaPila = new int[_longitudPila];
+
+        }
+        public bool PilaVacia()
+        {
+            if(_cima == -1)
+            {
+                return true;
+            }
+            else
+                return false;
+            //return _cima == -1?true:false;
+        }
+        public bool PilaLlena()
+        {
+            if(_cima == _longitudPila-1)
+            {
+                return true;
+            }
+            return false;
+            //return _cima == longitudPila - 1 ? true : false;
+        }
+        public int LongitudPila()
+        {
+            return _longitudPila;
+        }
+        public int CantidadElementos()
+        {
+            return _cima+1;
+        }
+        public int valorCima()
+        {
+            return listaPila[_cima];
+        }
+        public bool InsertarElemento(int valor)
+        {
+            if(PilaLlena())
+            {
+                return false;
+            }
+            else
+            {
+                _cima++;
+                listaPila[_cima] = valor;
+                return true;
+            }
+            
+        }
+        public int ExtraerElemento()
+        {
+            if (PilaVacia())
+            {
+                Console.WriteLine("Pila vacia");
+                return _cima;
+            }
+            else
+            {
+                int aux = listaPila[_cima];
+                _cima--;
+                Console.WriteLine($"El valor eliminado es: {aux}");
+                return aux;
+            }
+
+        }
+        public bool LimpiarPila()
+        {
+            if (PilaVacia())
+            {
+                return false;
+            }
+            else
+            {
+                while(!PilaVacia())
+                {
+                    ExtraerElemento();
+                }
+            }
+            return true;
+        }
+        public void ImprimirElementos()
+        {
+            if (PilaVacia())
+            {
+                Console.WriteLine("Pila vacia");
+            }
+            else
+            {
+                for (int i = _cima; i >= 0; i--)
+                {
+                    Console.Write(listaPila[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+   
+}
---
+++ EjercicioPilas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPilas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilas pilas = new Pilas();
+            int opcion = 0;
+            while(opcion != 5)
+            { 
+            Console.WriteLine("Selecciona una opcion\n" +
+                "1.- Insertar elemento\n"+
+                "2.- Eliminar elemento\n"+
+                "3.- Limpiar pila\n"+
+                "4.- Imprimir pila\n" +
+                "5.- Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch ( opcion )
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese un valor");
+                        int valor = Convert.ToInt32(Console.ReadLine());
+                        pilas.InsertarElemento(valor);
+                        break;
+                    case 2:
+                        pilas.ExtraerElemento();
+                        break;
+                    case 3:
+                        pilas.LimpiarPila();
+                        break;
+                    case 4:
+                        pilas.ImprimirElementos();
+                        break;
+                    case 5:
+                        break;
+                }
+            
+
+            } 
+        }
+    }
+}
